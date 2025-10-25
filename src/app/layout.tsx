4ac3f408b4dd6cbb417d8099a50f25c9191e929d
mina-spacev <minagwak@spacev.kr>
2025-10-25 15:17:45 +0900

    Chore: Nextra 프로젝트 생성

---
+++ src/app/layout.tsx
@@ -0,0 +1,54 @@
+import { Footer, Layout, Navbar } from 'nextra-theme-docs'
+import { Banner, Head } from 'nextra/components'
+import { getPageMap } from 'nextra/page-map'
+import { pretendard } from "@/styles/font";
+import 'nextra-theme-docs/style.css'
+import "@/styles/globals.css";
+import { ReactNode } from 'react';
+ 
+export const metadata = {
+  title: "Document Template",
+  description: "Document Template",
+}
+ 
+const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
+const navbar = (
+  <Navbar
+    logo={<b>Nextra</b>}
+    // ... Your additional navbar options
+  />
+)
+const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
+ 
+export default async function RootLayout({ children }: Readonly<{
+  children: ReactNode
+}>) {
+  return (
+    <html
+      // Not required, but good for SEO
+      lang="en"
+      // Required to be set
+      dir="ltr"
+      // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+      suppressHydrationWarning
+    >
+      <Head
+      // ... Your additional head options
+      >
+        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+      </Head>
+      <body className={pretendard.className}>
+        <Layout
+          banner={banner}
+          navbar={navbar}
+          pageMap={await getPageMap()}
+          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          footer={footer}
+          // ... Your additional layout options
+        >
+          {children}
+        </Layout>
+      </body>
+    </html>
+  )
+}
